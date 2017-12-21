3a90644fe18f427ac7e47a3059c54817963529ce
Quan Wang <BetterWang@gmail.com>
2017-12-21 04:38:40 +0100

    [+] update

--- test/crab_pPb16_HM_V0_reverse_v1.py
+++ test/crab_pPb16_HM_V0_reverse_v1.py
@@ -5,12 +5,12 @@ from httplib import HTTPException
 
 config = config()
 
-config.General.requestName = 'PAHM0_V0_reverse_v3'
+config.General.requestName = 'PAHM0_V0_reverse_v4'
 config.General.workArea = 'CrabArea'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'qw_pPb16_HM0_v3.py'
+config.JobType.psetName = 'qw_pPb16_HM0_v4.py'
 config.Data.inputDataset = '/PAHighMultiplicity0/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.JobType.inputFiles = ['Hijing_8TeV_dataBS.root']
 config.Data.inputDBS = 'phys03'
@@ -35,8 +35,8 @@ except ClientException as cle:
 ### 1
 config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 
-config.General.requestName = 'PAHM1_V0_reverse_v3'
-config.JobType.psetName = 'qw_pPb16_HM1_v3.py'
+config.General.requestName = 'PAHM1_V0_reverse_v4'
+config.JobType.psetName = 'qw_pPb16_HM1_v4.py'
 try:
         crabCommand('submit', config = config)
 except HTTPException as hte:
@@ -46,7 +46,7 @@ except ClientException as cle:
 
 ### 2
 config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.General.requestName = 'PAHM2_V0_reverse_v3'
+config.General.requestName = 'PAHM2_V0_reverse_v4'
 try:
         crabCommand('submit', config = config)
 except HTTPException as hte:
@@ -56,7 +56,7 @@ except ClientException as cle:
 
 ### 3
 config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.General.requestName = 'PAHM3_V0_reverse_v3'
+config.General.requestName = 'PAHM3_V0_reverse_v4'
 try:
         crabCommand('submit', config = config)
 except HTTPException as hte:
@@ -66,7 +66,7 @@ except ClientException as cle:
 
 ### 4
 config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.General.requestName = 'PAHM4_V0_reverse_v3'
+config.General.requestName = 'PAHM4_V0_reverse_v4'
 try:
         crabCommand('submit', config = config)
 except HTTPException as hte:
@@ -78,7 +78,7 @@ except ClientException as cle:
 
 ### 5
 config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.General.requestName = 'PAHM5_V0_reverse_v3'
+config.General.requestName = 'PAHM5_V0_reverse_v4'
 try:
         crabCommand('submit', config = config)
 except HTTPException as hte:
@@ -89,7 +89,7 @@ except ClientException as cle:
 
 ### 6
 config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.General.requestName = 'PAHM6_V0_reverse_v3'
+config.General.requestName = 'PAHM6_V0_reverse_v4'
 try:
         crabCommand('submit', config = config)
 except HTTPException as hte:
@@ -100,8 +100,8 @@ except ClientException as cle:
 
 ### 7
 config.Data.inputDataset = '/PAHighMultiplicity7/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.General.requestName = 'PAHM7_V0_reverse_v3'
-config.JobType.psetName = 'qw_pPb16_HM7_v3.py'
+config.General.requestName = 'PAHM7_V0_reverse_v4'
+config.JobType.psetName = 'qw_pPb16_HM7_v4.py'
 try:
         crabCommand('submit', config = config)
 except HTTPException as hte:
