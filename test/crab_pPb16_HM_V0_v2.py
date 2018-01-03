2a0bb050bf09b65bcf751dc144bf1ad2c5dea349
Quan Wang <BetterWang@gmail.com>
2018-01-03 18:56:56 +0100

    [+] update

---
+++ test/crab_pPb16_HM_V0_v2.py
@@ -0,0 +1,112 @@
+from CRABAPI.RawCommand import crabCommand
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+from CRABClient.ClientExceptions import ClientException
+from httplib import HTTPException
+
+config = config()
+
+config.General.requestName = 'PAHM0_V0_rap_v5'
+config.General.workArea = 'CrabArea'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'qw_pPb16_HM0_rap_v5.py'
+config.Data.inputDataset = '/PAHighMultiplicity0/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.JobType.inputFiles = ['Hijing_8TeV_dataBS.root']
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 20
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/qwang/CumuV0/'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/HI/Cert_285479-285832_HI8TeV_PromptReco_pPb_Collisions16_JSON_NoL1T.txt'
+config.Data.publication = False
+config.Data.useParent = True
+config.Site.storageSite = 'T2_CH_CERN'
+config.Site.ignoreGlobalBlacklist = True
+#config.Data.allowNonValidInputDataset = True
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
+
+### 1
+config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+
+config.General.requestName = 'PAHM1_V0_rap_v5'
+config.JobType.psetName = 'qw_pPb16_HM1_rap_v5.py'
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+### 2
+config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+config.General.requestName = 'PAHM2_V0_rap_v5'
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+### 3
+config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+config.General.requestName = 'PAHM3_V0_rap_v5'
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+### 4
+config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+config.General.requestName = 'PAHM4_V0_rap_v5'
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
+
+### 5
+config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+config.General.requestName = 'PAHM5_V0_rap_v5'
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
+### 6
+config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+config.General.requestName = 'PAHM6_V0_rap_v5'
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
+### 7
+config.Data.inputDataset = '/PAHighMultiplicity7/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+config.General.requestName = 'PAHM7_V0_rap_v5'
+config.JobType.psetName = 'qw_pPb16_HM7_rap_v5.py'
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
