3c1b152b128b0a049b033a3a1d5731735402b473
Quan Wang <BetterWang@gmail.com>
2020-02-21 18:13:46 +0100

    [+] update

--- run2018/crab_V0cumu_HIMB2018_Lm_Peak_Mid_BDT20_v1.py
+++ run2018/crab_V0cumu_HIMB2018_Lm_Peak_Mid_BDT20_v1.py
@@ -36,12 +36,25 @@ config.Data.allowNonValidInputDataset = True
 #
 
 config.JobType.psetName = 'qwcumu_PbPb18_V0_v2s.py'
-config.General.requestName = 'HIMB1_V0Cumu_Lm_Peak_Mid_BDT20_v1'
-config.Data.inputDataset = '/HIMinimumBias1/qwang-V0Skim_v3-5f932986cf38f9e8dbd6c3aea7f6c2b4/USER'
+#config.General.requestName = 'HIMB1_V0Cumu_Lm_Peak_Mid_BDT20_v1'
+#config.Data.inputDataset = '/HIMinimumBias1/qwang-V0Skim_v3-5f932986cf38f9e8dbd6c3aea7f6c2b4/USER'
+#try:
+#        crabCommand('submit', config = config)
+#except HTTPException as hte:
+#        print "Failed submitting task: %s" % (hte.headers)
+#except ClientException as cle:
+#        print "Failed submitting task: %s" % (cle)
+#
+
+config.General.requestName = 'HIMB2_V0Cumu_Lm_Peak_Mid_BDT20_v1'
+config.Data.inputDataset = '/HIMinimumBias2/qwang-V0Skim_v3-5f932986cf38f9e8dbd6c3aea7f6c2b4/USER'
 try:
         crabCommand('submit', config = config)
 except HTTPException as hte:
         print "Failed submitting task: %s" % (hte.headers)
 except ClientException as cle:
         print "Failed submitting task: %s" % (cle)
+
+
+
 
