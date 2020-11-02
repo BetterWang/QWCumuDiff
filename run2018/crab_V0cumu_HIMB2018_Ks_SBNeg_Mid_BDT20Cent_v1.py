f2a90ce27d93b4356417caec1f93ae202dafa0c1
Quan Wang <BetterWang@gmail.com>
2020-11-02 19:19:21 +0100

    [+] more crab

---
+++ run2018/crab_V0cumu_HIMB2018_Ks_SBNeg_Mid_BDT20Cent_v1.py
@@ -0,0 +1,37 @@
+from CRABAPI.RawCommand import crabCommand
+from CRABClient.UserUtilities import config
+from CRABClient.ClientExceptions import ClientException
+from httplib import HTTPException
+
+config = config()
+
+config.General.requestName = 'HIMB0_V0Cumu_Ks_SBNeg_Mid_BDT20Cent_v1'
+config.General.workArea = 'CrabArea'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'qwcumu_PbPb18_V0Cent_v2s.py'
+#config.JobType.maxJobRuntimeMin = 2500
+config.JobType.inputFiles = ['TMVAClassification_BDT250_D4.KS_1.weights.xml', 'TMVAClassification_BDT250_D4.KS_2.weights.xml', 'TMVAClassification_BDT250_D4.KS_3.weights.xml', 'TMVAClassification_BDT250_D4.KS_4.weights.xml']
+config.JobType.pyCfgParams = ['part=KS', 'massRange=SBNeg', 'rap=Mid']
+config.Data.inputDataset = '/HIMinimumBias0/qwang-V0Skim_v3-5f932986cf38f9e8dbd6c3aea7f6c2b4/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'Automatic'
+#config.Data.unitsPerJob = 8
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/qwang/PbPb2018'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/HI/PromptReco/Cert_326381-327564_HI_PromptReco_Collisions18_JSON.txt'
+config.Data.publication = False
+#config.Data.outputDatasetTag = ''
+config.Data.useParent = True
+config.Data.ignoreLocality = True
+config.Site.whitelist = ['T2_US_Vanderbilt']
+config.Site.storageSite = 'T2_CH_CERN'
+config.Data.allowNonValidInputDataset = True
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
---
+++ run2018/crab_V0cumu_HIMB2018_Lm_SBPos_Mid_BDT20Cent_v1.py
@@ -0,0 +1,76 @@
+from CRABAPI.RawCommand import crabCommand
+from CRABClient.UserUtilities import config
+from CRABClient.ClientExceptions import ClientException
+from httplib import HTTPException
+
+config = config()
+
+config.General.requestName = 'HIMB0_V0Cumu_Lm_SBPos_Mid_BDT20Cent_v1'
+config.General.workArea = 'CrabArea'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'qwcumu_PbPb18_V0Cent_v2s.py'
+#config.JobType.maxJobRuntimeMin = 2500
+config.JobType.inputFiles = ['TMVAClassification_BDT250_D4.LM_1.weights.xml', 'TMVAClassification_BDT250_D4.LM_2.weights.xml', 'TMVAClassification_BDT250_D4.LM_3.weights.xml', 'TMVAClassification_BDT250_D4.LM_4.weights.xml']
+config.JobType.pyCfgParams = ['part=LM', 'massRange=SBPos', 'rap=Mid', 'BDT=0.20']
+config.Data.inputDataset = '/HIMinimumBias0/qwang-V0Skim_v3-5f932986cf38f9e8dbd6c3aea7f6c2b4/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'Automatic'
+#config.Data.unitsPerJob = 8
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/qwang/PbPb2018'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/HI/PromptReco/Cert_326381-327564_HI_PromptReco_Collisions18_JSON.txt'
+config.Data.publication = False
+#config.Data.outputDatasetTag = ''
+config.Data.useParent = True
+config.Data.ignoreLocality = True
+config.Site.whitelist = ['T2_US_Vanderbilt']
+config.Site.storageSite = 'T2_CH_CERN'
+config.Data.allowNonValidInputDataset = True
+#try:
+#        crabCommand('submit', config = config)
+#except HTTPException as hte:
+#        print "Failed submitting task: %s" % (hte.headers)
+#except ClientException as cle:
+#        print "Failed submitting task: %s" % (cle)
+#
+#
+#config.General.requestName = 'HIMB1_V0Cumu_Lm_SBPos_Mid_BDT20Cent_v1'
+#config.Data.inputDataset = '/HIMinimumBias1/qwang-V0Skim_v3-5f932986cf38f9e8dbd6c3aea7f6c2b4/USER'
+#try:
+#        crabCommand('submit', config = config)
+#except HTTPException as hte:
+#        print "Failed submitting task: %s" % (hte.headers)
+#except ClientException as cle:
+#        print "Failed submitting task: %s" % (cle)
+#
+#
+#config.General.requestName = 'HIMB2_V0Cumu_Lm_SBPos_Mid_BDT20Cent_v1'
+#config.Data.inputDataset = '/HIMinimumBias2/qwang-V0Skim_v3-5f932986cf38f9e8dbd6c3aea7f6c2b4/USER'
+#try:
+#        crabCommand('submit', config = config)
+#except HTTPException as hte:
+#        print "Failed submitting task: %s" % (hte.headers)
+#except ClientException as cle:
+#        print "Failed submitting task: %s" % (cle)
+#
+#config.General.requestName = 'HIMB3_V0Cumu_Lm_SBPos_Mid_BDT20Cent_v1'
+#config.Data.inputDataset = '/HIMinimumBias3/qwang-V0Skim_v3-5f932986cf38f9e8dbd6c3aea7f6c2b4/USER'
+#try:
+#        crabCommand('submit', config = config)
+#except HTTPException as hte:
+#        print "Failed submitting task: %s" % (hte.headers)
+#except ClientException as cle:
+#        print "Failed submitting task: %s" % (cle)
+#
+#
+config.General.requestName = 'HIMB5_V0Cumu_Lm_SBPos_Mid_BDT20Cent_v1'
+config.Data.inputDataset = '/HIMinimumBias5/qwang-V0Skim_v3-5f932986cf38f9e8dbd6c3aea7f6c2b4/USER'
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
